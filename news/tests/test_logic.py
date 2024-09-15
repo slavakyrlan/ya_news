2f886df866e6024fe9ad7ef9e9aae7e6e1e93e40
slavakyrlan <slavakyrlan00@mail.ru>
2024-09-15 15:56:14 +0300

    add tests

---
+++ news/tests/test_logic.py
@@ -0,0 +1,20 @@
+# Импортируем функцию для определения модели пользователя.
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+
+# Получаем модель пользователя.
+User = get_user_model()
+
+
+class TestNews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаём пользователя.
+        cls.user = User.objects.create(username='testUser')
+        # Создаём объект клиента.
+        cls.user_client = Client()
+        # "Логинимся" в клиенте при помощи метода force_login().
+        cls.user_client.force_login(cls.user)
+        # Теперь через этот клиент можно отправлять запросы
+        # от имени пользователя с логином "testUser".
